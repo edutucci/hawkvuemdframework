eb83c96251f402e093692fe505efe0433f86b648
Eduardo Tucci <edutucci@gmail.com>
2018-06-12 23:38:12 -0300

    adding tutorial

--- src/pages/list.vue
+++ src/pages/list.vue
@@ -1,4 +1,4 @@
-<template>
+<template lang="pug">
   <div>
     <h1>List</h1>
   
@@ -29,6 +29,12 @@
           </h-list-body>           
         </h-list>
       </div>
+    .boxshadow
+      <pre v-highlightjs="list">
+        <code class="html">
+        </code>
+      </pre>  
+
       <div class="col-6">
         <h-list>
           <h-app-toolbar>
@@ -53,8 +59,11 @@
             <h-list-item-left :icon="['fas', 'plane']" text="Travel"/>
           </h-list-item>
         </h-list>
-      </div>   
-   
+      </div>  
+    <pre v-highlightjs="list2">
+      <code class="html">
+      </code>
+    </pre>    
 
       <h-list class="col-6">
         <h-app-toolbar>
@@ -72,6 +81,11 @@
         </h-list-item>          
       </h-list>
 
+    <pre v-highlightjs="list3">
+      <code class="html">
+      </code>
+    </pre>        
+      
       <h-list class="col-6">
         <h-app-toolbar>
           <h-app-toolbar-container bgcolor="bg-primary" textcolor="text-white">
@@ -79,7 +93,7 @@
             <h-app-toolbar-title title="My files" style="padding-top: 40px;"/>
           </h-app-toolbar-container>
         </h-app-toolbar>        
-        <h-list-header middletext="Folders for my friend"/>
+        <h-list-header middletext="Folders"/>
         <h-list-item>
           <h-list-item-left img="/static/avatar/folder_open.jpeg" text="Photos" desc="Dez 12, 2017"/>
         </h-list-item>
@@ -87,7 +101,11 @@
           <h-list-item-left img="/static/avatar/folder_open.jpeg" text="Images" desc="Events"/>
         </h-list-item>
       </h-list>
-  
+    <pre v-highlightjs="list4">
+      <code class="html">
+      </code>
+    </pre>  
+      
       <h-list class="col-6">
         <h-list-header text="4 items in your cart" />
         <h-list-item separator>
@@ -107,6 +125,11 @@
           <h-list-item-right text="8.00"/>
         </h-list-item>
       </h-list>
+    
+    <pre v-highlightjs="list5">
+      <code class="html">
+      </code>
+    </pre>    
   
   </div>
 </template>
@@ -142,7 +165,118 @@ export default {
   },
   data () {
     return {
-      topicItem: ''
+      topicItem: '',
+      list: `
+<div class="col-6">
+  <h-list>
+    <h-app-toolbar>
+      <h-app-toolbar-container bgcolor="bg-primary" textcolor="text-white">
+        <h-app-toolbar-navigation :icon="['fas', 'bars']"/>
+        <h-app-toolbar-title title="Topics"/>
+      </h-app-toolbar-container>
+    </h-app-toolbar>
+    <h-list-body>
+      <h-list-item @click="topicItem = 'Attractions'" :active="topicItem === 'Attractions'" >
+        <h-list-item-left lefttext="Attractions" />
+      </h-list-item>
+      <h-list-item @click="topicItem = 'Dining'" :active="topicItem === 'Dining'">
+        <h-list-item-left lefttext="Dining"/>
+      </h-list-item>
+      <h-list-item @click="topicItem = 'Health'" :active="topicItem === 'Health'">
+        <h-list-item-left lefttext="Health"/>
+      </h-list-item>
+      <h-list-item @click="topicItem = 'Sports'" :active="topicItem === 'Sports'">
+        <h-list-item-left lefttext="Sports"/>
+      </h-list-item>
+      <h-list-item @click="topicItem = 'Travel'" :active="topicItem === 'Travel'">
+        <h-list-item-left lefttext="Travel"/>
+      </h-list-item> 
+    </h-list-body>           
+  </h-list>
+</div>      
+      `,
+      list2: `
+<div class="col-6">
+  <h-list>
+    <h-app-toolbar>
+      <h-app-toolbar-container bgcolor="bg-primary" textcolor="text-white">
+        <h-app-toolbar-navigation :icon="['fas', 'bars']"/>
+        <h-app-toolbar-title title="Topics"/>
+      </h-app-toolbar-container>
+    </h-app-toolbar>
+    <h-list-item @click="topicItem = 'Attractions'" :active="topicItem === 'Attractions'">
+      <h-list-item-left :icon="['fas', 'film']" text="Attractions"/>
+    </h-list-item>
+    <h-list-item @click="topicItem = 'Dining'" :active="topicItem === 'Dining'" >
+      <h-list-item-left :icon="['fas', 'utensils']" text="Dining"/>
+    </h-list-item>
+    <h-list-item @click="topicItem = 'Health'" :active="topicItem === 'Health'">
+      <h-list-item-left :icon="['fas', 'heart']" text="Health"/>
+    </h-list-item>
+    <h-list-item @click="topicItem = 'Sports'" :active="topicItem === 'Sports'">
+      <h-list-item-left :icon="['fas', 'basketball-ball']" text="Sports"/>
+    </h-list-item>
+    <h-list-item @click="topicItem = 'Travel'" :active="topicItem === 'Travel'">
+      <h-list-item-left :icon="['fas', 'plane']" text="Travel"/>
+    </h-list-item>
+  </h-list>
+</div> 
+      `,
+      list3: `
+<h-list class="col-6">
+  <h-app-toolbar>
+    <h-app-toolbar-container bgcolor="bg-primary" textcolor="text-white">
+      <h-app-toolbar-navigation :icon="['fas', 'bars']"/>
+      <h-app-toolbar-title title="Inbox"/>
+    </h-app-toolbar-container>
+  </h-app-toolbar>
+  <h-list-header text="Today is my birthday"/>
+  <h-list-item>
+    <h-list-item-left separator avatar="/static/avatar/turtle.png" text="Hi rambo" desc="You there?" />
+  </h-list-item>
+  <h-list-item>
+    <h-list-item-left separator avatar="/static/avatar/rambo.jpg" text="I am searching something to eat" desc="I think I am going to eat you."/>
+  </h-list-item>          
+</h-list>
+      `,
+      list4: `
+      <h-list class="col-6">
+        <h-app-toolbar>
+          <h-app-toolbar-container bgcolor="bg-primary" textcolor="text-white">
+            <h-app-toolbar-navigation :icon="['fas', 'bars']"/>
+            <h-app-toolbar-title title="My files" style="padding-top: 40px;"/>
+          </h-app-toolbar-container>
+        </h-app-toolbar>        
+        <h-list-header middletext="Folders"/>
+        <h-list-item>
+          <h-list-item-left img="/static/avatar/folder_open.jpeg" text="Photos" desc="Dez 12, 2017"/>
+        </h-list-item>
+        <h-list-item>
+          <h-list-item-left img="/static/avatar/folder_open.jpeg" text="Images" desc="Events"/>
+        </h-list-item>
+      </h-list>
+      `,
+      list5: `
+<h-list class="col-6">
+  <h-list-header text="4 items in your cart" />
+  <h-list-item separator>
+    <h-list-item-left img="/static/img/pencil.png" text="Pencil" desc="in stock" />
+    <h-list-item-right text="2.00"/>
+  </h-list-item>
+  <h-list-item separator>
+    <h-list-item-left img="/static/img/rubberbands.png" text="Hubberbands" desc="in stock" />
+    <h-list-item-right text="4.00"/>
+  </h-list-item>
+  <h-list-item separator>
+    <h-list-item-left img="/static/img/rulers.png" text="Rulers" desc="only 1 left in stock. Next month we have to sell." />
+    <h-list-item-right text="6.00"/>
+  </h-list-item>
+  <h-list-item separator>
+    <h-list-item-left img="/static/img/clock.png" text="Clock" desc="in stock" />
+    <h-list-item-right text="8.00"/>
+  </h-list-item>
+</h-list>
+      `
     }
   },
   created () {
