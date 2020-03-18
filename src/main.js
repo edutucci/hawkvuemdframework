79b929ff2076003f2b166c2b93b112df2d6ab8a3
edutucci <edutucci@gmail.com>
2020-03-18 12:26:44 -0300

    updates

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+
+// import './index.styl' // custom color
 import './theme/index.styl'
-import './theme/app.styl'
+import './app.styl'
 import './index.js'
 
 // Vue Prism.js
